665e2b8690a2f0c6de01aa0ab0a4412f2c4e540b
fledgless <cameriancardinal@gmail.com>
2025-03-17 14:45:31 +0100

    first commit

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Erreur de connexion à la base de données:', err);
+        process.exit(1);
+    } else {
+        console.log('Bien connecté à la base de données');
+    }
+});
+module.exports = db;
