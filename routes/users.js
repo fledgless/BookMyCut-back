3ffb768ecddd51d419a9e4b09d0f3a450bb3ea0e
Slyxi7 <riem.baptiste0705@gmail.com>
2025-03-25 11:27:47 +0100

    Ajout de la route /login avec connexion soit par pseudo ou par email

--- routes/users.js
+++ routes/users.js
@@ -12,12 +12,9 @@ router.post("/register", async (req, res) => {
   const { username, email, password, prenom, nomFamille } = req.body;
 
   if (!username || !email || !password || !prenom || !nomFamille) {
-    return res
-      .status(400)
-      .json({
-        message:
-          "Données manquantes : username, password et email sont requis.",
-      });
+    return res.status(400).json({
+      message: "Données manquantes : username, password et email sont requis.",
+    });
   }
 
   try {
@@ -79,48 +76,53 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   const { usernameOrEmail, password } = req.body;
 
-  const sql = "SELECT * FROM users WHERE email = ? OR username = ?";
+  if (!usernameOrEmail || !password) {
+    return res
+      .status(400)
+      .send({ message: "Nom d'utilisateur/email et mot de passe sont requis" });
+  }
+
+  const sql = "SELECT * FROM Users WHERE email = ? OR username = ?";
 
   connection.query(
     sql,
     [usernameOrEmail, usernameOrEmail],
     async (err, results) => {
       if (err) {
+        console.error("Erreur dans la requête SQL:", err);
         return res.status(500).send({ message: "Erreur lors de la connexion" });
       }
 
-      if (
-        results.length === 0 ||
-        !(await bcrypt.compare(password, results[0].password))
-      ) {
+      if (results.length === 0) {
+        console.log("Aucun utilisateur trouvé avec cet email ou pseudo.");
+        return res
+          .status(401)
+          .send({ message: "Nom d'utilisateur ou mot de passe incorrect" });
+      }
+
+      const user = results[0];
+
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordValid) {
         console.log(
-          password,
-          "ici le password",
-          "ici le mdp décrypté :",
-          results[0].password + results[0].email
+          "Mot de passe incorrect pour l'utilisateur :",
+          usernameOrEmail
         );
         return res
           .status(401)
           .send({ message: "Nom d'utilisateur ou mot de passe incorrect" });
       }
 
-      const user = {
-        id: results[0].user_id,
-        username: results[0].username,
-        email: results[0].email,
-        prenom: results[0].prenom,
-        nomFamille: results[0].nomFamille,
-      };
-
       const token = jwt.sign(
         {
-          id: user.id,
+          id: user.user_id,
           username: user.username,
           email: user.email,
           prenom: user.prenom,
           nomFamille: user.nomFamille,
         },
-        "secretKey",
+        process.env.JWT_SECRET,
         { expiresIn: "1d" }
       );
 
